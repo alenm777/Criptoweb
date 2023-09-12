354dd791cf6799ca1851fedfb3053b5e1bf4c442
alenm777 <alenm.1717@gmail.com>
2023-09-12 11:29:55 -0300

    tendencias

--- criptoweb/src/routes/Inicio.jsx
+++ criptoweb/src/routes/Inicio.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BuscadorMoneda from '../components/BuscadorMoneda'
+import Tendencias from '../components/Tendencias'
 
 const Inicio = ({coins}) => {
   return (
     <div>
       <BuscadorMoneda coins={coins} />
+      <Tendencias />
     </div>
   )
 }
